5f7011761f38f6f1804c95200d5113fe1add7533
Mihir <mihirkantho@gmail.com>
2021-06-28 19:16:31 +0600

    final

--- app.js
+++ app.js
@@ -16,10 +16,34 @@ let weather = {
         const { icon, description } = data.weather[0];
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log( name,icon,description,temp,humidity,speed);
+       
     
         document.querySelector(".city") .innerText = "Weather in " + name;
-        document.querySelector(".icon") .src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".tempr").innerText = temp + "°C" ;
+        document.querySelector(".icon") .src = "https://openweathermap.org/img/wn/" + icon + ".png"
+        document.querySelector(".description") .innerText = description;
+        document.querySelector(".humidity") .innerText = "Humidity: " + humidity + "%" ;
+        document.querySelector(".wind") .innerText = "Wind Speed: " + speed + "km/h";
+    },
+    search: function(){
+        this.fetchWeather (document.querySelector(".search-bar").value);
     }
 };
+
+document.querySelector(".search button")
+.addEventListener("click", function(){
+    weather.search();
+
+});
+
+document.querySelector(".search-bar")
+.addEventListener("keyup", function(event){
+
+    if (event.key == "Enter"){
+        weather.search();
+    }
+
+});
+
+
 
